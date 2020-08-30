2953e162d9a9797b782ad1f4a89d0c85f26462b1
Sonal Singh <sonal.singh.19993@gmail.com>
2020-08-30 02:50:20 +0000

    sm6250-common: Add MetroMusicPlayer
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- common.mk
+++ common.mk
@@ -185,6 +185,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.recovery.qcom.rc:recovery/root/init.recovery.qcom.rc \
     $(LOCAL_PATH)/rootdir/bin/init.recovery.qcom.sh:recovery/root/init.recovery.qcom.sh
 
+# RetroMusicPlayer
+PRODUCT_PACKAGES += \
+    RetroMusicPlayer
+
 # Sensors
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
