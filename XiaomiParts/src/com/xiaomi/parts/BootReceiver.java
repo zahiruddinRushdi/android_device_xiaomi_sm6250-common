74fc1c7f98dd3d116595b92f76e1b07b527677d4
Kshitij Gupta <kshitijgm@gmail.com>
2020-09-20 18:05:35 +0530

    sm6250-common: XiaomiParts: Refactor Dirac to Mi, for MiSoundFX
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- XiaomiParts/src/com/xiaomi/parts/BootReceiver.java
+++ XiaomiParts/src/com/xiaomi/parts/BootReceiver.java
@@ -130,8 +130,8 @@ public class BootReceiver extends BroadcastReceiver implements Utils {
                 DeviceSettings.PREF_USB_FASTCHARGE, 0));
         FileUtils.setValue(DeviceSettings.MSM_TOUCHBOOST_PATH, Settings.Secure.getInt(context.getContentResolver(),
                 DeviceSettings.PREF_MSM_TOUCHBOOST, 0));
-        // Dirac
-        context.startService(new Intent(context, DiracService.class));
+        // Mi
+        context.startService(new Intent(context, MiService.class));
 
        // Ambient
         context.startService(new Intent(context, SensorsDozeService.class));
