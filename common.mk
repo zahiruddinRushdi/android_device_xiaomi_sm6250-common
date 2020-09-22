b02ec8998c6d97122f8c8e542805b1d2dc27498a
Sonal Singh <sonal.singh.19993@gmail.com>
2020-09-22 22:48:15 +0530

    sm6250-common: Add pixel bootanimation from sunfish

--- common.mk
+++ common.mk
@@ -38,6 +38,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
+# Bootanimation
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
