aa251f537977954d511f39147d8564f256cd51e4
Sonal Singh <sonal.singh.19993@gmail.com>
2020-09-23 19:29:38 +0530

    sm6250-common: Build PixelLiveWallpaper
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,3 @@
 export SHIP_LAWNCHAIR=true
 export SELINUX_IGNORE_NEVERALLOWS=true
+git clone https://gitlab.com/PixelExperience/packages_apps_PixelLiveWallpaper.git -b ten packages/apps/PixelLiveWallpaper
